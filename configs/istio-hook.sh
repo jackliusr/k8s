0b741b896b01752ee30a989d402ca5c5b248a8a4
Jack Liu S. R <jackliusr@gmail.com>
2021-02-04 10:18:07 +0800

    istio lab env

---
+++ configs/istio-hook.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
+kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+# docker network inspect kind | jq '.[0].IPAM.Config[0].Subnet' 
+#    127.18.0.0/16
+# so reserver the range 172.18.255.1-172.18.255.250 for load balancer service
+SUBNET=$( docker inspect --type network kind | jq -r '.[0].IPAM.Config[0].Subnet | split(".") | .[0:2] | join(".")')
+cat <<EOF | kubectl apply -f - 
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: metallb-system
+  name: config
+data:
+  config: |
+    address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+      - ${SUBNET}.255.1-${SUBNET}.255.250
+EOF
+
+istioctl install --set profile=demo -y
+
+
