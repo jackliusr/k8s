250b23529dcb86b8d0ca0c6802e1f0f1d4572245
Jack Liu Shurui <jackliusr@gmail.com>
2021-09-13 20:54:19 +0800

    check if cluster is successfully created

--- up.sh
+++ up.sh
@@ -6,11 +6,13 @@ if [ -n "$result" ]; then
 else
   echo "no exists"
   kind create cluster --config ./configs/$1-cluster.yaml
+  if [ $? -eq 0 ] ; then
+    if [[ -f ./configs/$1-hook.sh ]]; then 
+      source ./configs/$1-hook.sh
+    fi
 
-  if [[ -f ./configs/$1-hook.sh ]]; then 
-     source ./configs/$1-hook.sh
+    docker exec -d kind-control-plane  /mnt/configs/common/kubeconfig-config.sh
+  else
+    echo "failed to create cluster"
   fi
-
-  docker exec -d kind-control-plane  /mnt/configs/common/kubeconfig-config.sh
-  
 fi 
