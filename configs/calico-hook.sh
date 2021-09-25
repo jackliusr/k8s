bc01de97b0eb097cd1f2024439db57c4e235c54b
Jack Liu Shurui <jackliusr@gmail.com>
2021-09-25 13:10:45 +0800

    1.22 with calico 3.19

--- configs/calico-hook.sh
+++ configs/calico-hook.sh
@@ -1,3 +1,3 @@
 #!/bin/sh  
-kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.19/manifests/calico.yaml
 kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
