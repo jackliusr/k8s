4528128f674109700ff326992e091325004aab53
Jack Liu S. R <jackliusr@gmail.com>
2023-07-18 20:15:05 +0800

    init env for k8sug dapr exploration

---
+++ configs/k8sugdapr-hook.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
+kubectl wait --namespace metallb-system \
+                --for=condition=ready pod \
+                --selector=app=metallb \
+                --timeout=90s
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+# docker network inspect kind | jq '.[0].IPAM.Config[0].Subnet' 
+#    127.18.0.0/16
+# so reserver the range 172.18.255.1-172.18.255.250 for load balancer service
+SUBNET=$( docker inspect --type network kind | jq -r '.[0].IPAM.Config[0].Subnet | split(".") | .[0:2] | join(".")')
+kubectl apply -f https://kind.sigs.k8s.io/examples/loadbalancer/metallb-config.yaml
+
+cat <<EOF | kubectl apply -f -
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: example
+  namespace: metallb-system
+spec:
+  addresses:
+   - ${SUBNET}.255.1-${SUBNET}.255.250
+---
+apiVersion: metallb.io/v1beta1
+kind: L2Advertisement
+metadata:
+  name: empty
+  namespace: metallb-system
+EOF
