db6d4201e5578faba790aab364c3dff944b3ec40
Jack Liu S. R <jackliusr@gmail.com>
2023-07-23 01:12:51 +0800

    reverse the IP change

--- configs/etcdsharding-hook.sh
+++ configs/etcdsharding-hook.sh
@@ -4,5 +4,5 @@ docker cp kind-control-plane:/etc/kubernetes/pki/etcd/ca.key ./configs/etcd/open
 docker cp kind-control-plane:/etc/kubernetes/pki/etcd/ca.crt ./configs/etcd/openssl/ca.crt
 docker exec -it kind-worker /mnt/configs/etcd/openssl/gen-key-cert.sh
 docker cp kind-control-plane:/etc/kubernetes/manifests/etcd.yaml /tmp/etcd.yaml
-sed -i -e 's/172.18.0.2/172.18.0.3/g' /tmp/etcd.yaml
+sed -i -e 's/172.18.0.3/172.18.0.2/g' /tmp/etcd.yaml
 docker cp /tmp/etcd.yaml  kind-worker:/etc/kubernetes/manifests/etcd.yaml
