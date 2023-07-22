4106ceee0f3b61ec0ae44278f2424289e8a68617
Jack Liu S. R <jackliusr@gmail.com>
2023-07-23 00:05:43 +0800

    etcd sharding env

--- configs/etcd/openssl/gen-key-cert.sh
+++ configs/etcd/openssl/gen-key-cert.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
+cd "$(dirname "$0")"
+pwd
+openssl genrsa -out server.key 2048
+openssl req -new -key server.key -out server.csr -config server.conf
+openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key \
+    -CAcreateserial -out server.crt -days 10000 \
+    -extensions v3_ext -extfile server.conf -sha256
 
-openssl req -newkey rsa:2048 -nodes -keyout server.key \
- -extensions v3_ext -extfile server.conf \
-  -x509 -out [filepath to cert]
+openssl genrsa -out peer.key 2048
+openssl req -new -key peer.key -out peer.csr -config server.conf
+openssl x509 -req -in peer.csr -CA ca.crt -CAkey ca.key \
+    -CAcreateserial -out peer.crt -days 10000 \
+    -extensions v3_ext -extfile server.conf -sha256
+
+openssl genrsa -out healthcheck-client.key 2048
+openssl req -new -key healthcheck-client.key -out healthcheck-client.csr -config server.conf \
+   -subj '/O=system:masters/CN=kube-etcd-healthcheck-client'
+openssl x509 -req -in healthcheck-client.csr -CA ca.crt -CAkey ca.key \
+    -CAcreateserial -out healthcheck-client.crt -days 10000 \
+    -extensions v3_ext -extfile server.conf -sha256
+    
+cp ca.crt /etc/kubernetes/pki/etcd
+cp {server,peer,healthcheck-client}.{key,crt} /etc/kubernetes/pki/etcd
--- configs/etcdsharding-hook.sh
+++ configs/etcdsharding-hook.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 docker exec -it kind-worker sh -c 'mkdir -p /etc/kubernetes/pki/etcd'
-docker cp kind-control-plane:/etc/kubernetes/pki/etcd/ca.key /tmp/ca.key
-docker cp kind-control-plane:/etc/kubernetes/pki/etcd/ca.crt /tmp/ca.crt
-docker cp /tmp/ca.key kind-worker:/etc/kubernetes/pki/etcd/ca.key
-docker cp /tmp/ca.crt kind-worker:/etc/kubernetes/pki/etcd/ca.crt
-docker exec -it kind-worker /mnt/configs/common/gen-etcd-certs.sh
+docker cp kind-control-plane:/etc/kubernetes/pki/etcd/ca.key ./configs/etcd/openssl/ca.key
+docker cp kind-control-plane:/etc/kubernetes/pki/etcd/ca.crt ./configs/etcd/openssl/ca.crt
+docker exec -it kind-worker /mnt/configs/etcd/openssl/gen-key-cert.sh
+docker cp kind-control-plane:/etc/kubernetes/manifests/etcd.yaml /tmp/etcd.yaml
+sed -i -e 's/172.18.0.2/172.18.0.3/g' /tmp/etcd.yaml
+docker cp /tmp/etcd.yaml  kind-worker:/etc/kubernetes/manifests/etcd.yaml
