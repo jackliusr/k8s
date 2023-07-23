7fa865e163da96c54020984cbd106bb118b58744
Jack Liu S. R <jackliusr@gmail.com>
2023-07-23 21:01:53 +0800

    cfssl version : etcd sharding

---
+++ configs/etcd/cfssl/gen-key-cert.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+pwd
+cp ../openssl/ca* . 
+curl -L https://github.com/cloudflare/cfssl/releases/download/v1.5.0/cfssl_1.5.0_linux_amd64 -o cfssl
+chmod +x cfssl
+curl -L https://github.com/cloudflare/cfssl/releases/download/v1.5.0/cfssljson_1.5.0_linux_amd64 -o cfssljson
+chmod +x cfssljson
+curl -L https://github.com/cloudflare/cfssl/releases/download/v1.5.0/cfssl-certinfo_1.5.0_linux_amd64 -o cfssl-certinfo
+chmod +x cfssl-certinfo
+mv cfssl* /usr/local/bin
+
+IP=$(hostname -i |sed 's/[^ ]* //')
+
+sh -c "sed -i -e 's/172.18.0.2/IP/g' server-csr.json"
+sh -c "sed -i -e 's/172.18.0.2/IP/g' healthcheck-client-csr.json"
+
+cfssl gencert -ca=ca.crt -ca-key=ca.key \
+     --config=ca-config.json -profile=server \
+     server-csr.json | cfssljson -bare server
+mv server-key.pem /etc/kubernetes/pki/etcd/server.key
+mv server.pem /etc/kubernetes/pki/etcd/server.crt
+
+cfssl gencert -ca=ca.crt -ca-key=ca.key \
+     --config=ca-config.json -profile=server \
+     server-csr.json | cfssljson -bare peer   
+mv peer-key.pem /etc/kubernetes/pki/etcd/peer.key
+mv peer.pem /etc/kubernetes/pki/etcd/peer.crt
+
+cfssl gencert -ca=ca.crt -ca-key=ca.key \
+     --config=ca-config.json -profile=server \
+     healthcheck-client-csr.json | cfssljson -bare healthcheck-client
+mv healthcheck-client-key.pem /etc/kubernetes/pki/etcd/healthcheck-client.key
+mv healthcheck-client.pem /etc/kubernetes/pki/etcd/healthcheck-client.crt
--- configs/etcdsharding-hook.sh
+++ configs/etcdsharding-hook.sh
@@ -2,7 +2,11 @@
 docker exec -it kind-worker sh -c 'mkdir -p /etc/kubernetes/pki/etcd'
 docker cp kind-control-plane:/etc/kubernetes/pki/etcd/ca.key ./configs/etcd/openssl/ca.key
 docker cp kind-control-plane:/etc/kubernetes/pki/etcd/ca.crt ./configs/etcd/openssl/ca.crt
-docker exec -it kind-worker /mnt/configs/etcd/openssl/gen-key-cert.sh
+# valid option: openssl,cfssl
+docker exec -it kind-worker /mnt/configs/etcd/cfssl/gen-key-cert.sh 
 docker cp kind-control-plane:/etc/kubernetes/manifests/etcd.yaml /tmp/etcd.yaml
-sed -i -e 's/172.18.0.3/172.18.0.2/g' /tmp/etcd.yaml
+
+CTL_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kind-control-plane)
+WORKER_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kind-worker)
+sh -c "sed -i -e 's/${CTL_IP}/${WORKER_IP}/g' /tmp/etcd.yaml"
 docker cp /tmp/etcd.yaml  kind-worker:/etc/kubernetes/manifests/etcd.yaml
