d7804cbadae7b5157e5704ab35b0b9753bc5729a
Jack Liu S. R <jackliusr@gmail.com>
2024-02-13 22:21:04 +0800

    metallb upgraded to v0.13.7 from v0.9.5

--- up.sh
+++ up.sh
@@ -2,17 +2,27 @@
 
 result=$( kind get clusters | grep -E '^kind$')
 if [ -n "$result" ]; then 
-  echo "exist"
-else
-  echo "no exists"
-  kind create cluster --config ./configs/$1-cluster.yaml
-  if [ $? -eq 0 ] ; then
-    if [[ -f ./configs/$1-hook.sh ]]; then 
-      source ./configs/$1-hook.sh
-    fi
+  echo "the kind cluster existed. Do you want to delete it (y/n)?"
+  read -p "Is this a good question (y/n)? " answer
+  case ${answer:0:1} in
+      y|Y )
+          kind delete cluster
+      ;;
+      * )
+          return
+      ;;
+  esac  
+fi
 
-    docker exec -d kind-control-plane  /mnt/configs/common/kubeconfig-config.sh
-  else
-    echo "failed to create cluster"
+
+kind create cluster --config ./configs/$1-cluster.yaml
+if [ $? -eq 0 ] ; then
+  if [[ -f ./configs/$1-hook.sh ]]; then 
+    source ./configs/$1-hook.sh
   fi
-fi 
+
+  docker exec -d kind-control-plane  /mnt/configs/common/kubeconfig-config.sh
+else
+  echo "failed to create cluster"
+fi
+ 
