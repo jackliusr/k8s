008694f6e24515d8382f679bc22793aab216a7b9
Jack Liu S. R <jackliusr@gmail.com>
2024-02-13 21:49:31 +0800

    upgrade to v3.27.0 from v3.8 for calico lab

--- configs/calico-hook.sh
+++ configs/calico-hook.sh
@@ -1,3 +1,3 @@
 #!/bin/sh  
-kubectl apply -f https://docs.projectcalico.org/v3.19/manifests/calico.yaml
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/calico.yaml
 kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
