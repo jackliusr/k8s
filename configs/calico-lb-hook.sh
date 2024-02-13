30753f61fcc7e99e3956ef477f1264c32a3cc031
Jack Liu S. R <jackliusr@gmail.com>
2024-02-13 21:47:38 +0800

    upgrade to calico v3.27.0 and metallb v0.13.7

--- configs/calico-lb-hook.sh
+++ configs/calico-lb-hook.sh
@@ -1,29 +1,34 @@
 #!/bin/bash 
-kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/calico.yaml
 kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+sleep 2
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
+kubectl wait --namespace metallb-system \
+                --for=condition=ready pod \
+                --selector=app=metallb \
+                --timeout=90s
 # On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 # docker network inspect kind | jq '.[0].IPAM.Config[0].Subnet' 
 #    127.18.0.0/16
 # so reserver the range 172.18.255.1-172.18.255.250 for load balancer service
 SUBNET=$( docker inspect --type network kind | jq -r '.[0].IPAM.Config[0].Subnet | split(".") | .[0:2] | join(".")')
-cat <<EOF | kubectl apply -f - 
-apiVersion: v1
-kind: ConfigMap
+kubectl apply -f https://kind.sigs.k8s.io/examples/loadbalancer/metallb-config.yaml
+cat <<EOF | kubectl apply -f -
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
 metadata:
+  name: example
   namespace: metallb-system
-  name: config
-data:
-  config: |
-    address-pools:
-    - name: default
-      protocol: layer2
-      addresses:
-      - ${SUBNET}.255.1-${SUBNET}.255.250
+spec:
+  addresses:
+   - ${SUBNET}.255.1-${SUBNET}.255.250
+---
+apiVersion: metallb.io/v1beta1
+kind: L2Advertisement
+metadata:
+  name: empty
+  namespace: metallb-system
 EOF
-
 sleep 5
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.43.0/deploy/static/provider/cloud/deploy.yaml
-
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/cloud/deploy.yaml
