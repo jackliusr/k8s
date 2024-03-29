fede236da71b86f23bdbbb932000d1b80f7dc729
Jack Liu Shurui <jackliusr@gmail.com>
2024-03-29 12:33:09 +0800

    workable cilium cluster configuration

--- configs/cilium-hook.sh
+++ configs/cilium-hook.sh
@@ -3,17 +3,34 @@ docker exec -d kind-control-plane /mnt/cni/cni-install.sh
 docker exec -d kind-worker/mnt/cni/cni-install.sh
 docker exec -d kind-worker2 /mnt/cni/cni-install.sh
 
-helm repo add cilium https://helm.cilium.io/
-docker pull cilium/cilium:v1.9.1
-kind load docker-image cilium/cilium:v1.9.1
-helm install cilium cilium/cilium --version 1.9.1 \
-   --namespace kube-system \
-   --set nodeinit.enabled=true \
-   --set kubeProxyReplacement=partial \
-   --set hostServices.enabled=false \
-   --set externalIPs.enabled=true \
-   --set nodePort.enabled=true \
-   --set hostPort.enabled=true \
-   --set bpf.masquerade=false \
-   --set image.pullPolicy=IfNotPresent \
-   --set ipam.mode=kubernetes
+helm upgrade --install --namespace kube-system --repo https://helm.cilium.io cilium cilium --values - <<EOF
+kubeProxyReplacement: strict
+k8sServiceHost: kind-control-plane
+k8sServicePort: 6443
+hostServices:
+  enabled: false
+externalIPs:
+  enabled: true
+nodePort:
+  enabled: true
+hostPort:
+  enabled: true
+image:
+  pullPolicy: IfNotPresent
+ipam:
+  mode: kubernetes
+hubble:
+  enabled: true
+  relay:
+    enabled: true
+  ui:
+    enabled: true
+    ingress:
+      enabled: true
+      annotations:
+        kubernetes.io/ingress.class: nginx
+      hosts:
+        - hubble-ui.127.0.0.1.nip.io
+EOF
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.0/deploy/static/provider/kind/deploy.yaml
