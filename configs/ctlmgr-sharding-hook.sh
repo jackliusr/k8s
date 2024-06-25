9ffca8b652a1b1b66b8030503e57d98f08ad5732
Jack Liu Shurui <jackliusr@gmail.com>
2024-06-25 22:53:11 +0800

    controller-manager sharding

---
+++ configs/ctlmgr-sharding-hook.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+sleep 3m
+docker cp ./configs/controller-mgr/kube-controller-manager-1.yaml kind-control-plane:/etc/kubernetes/manifests/kube-controller-manager-1.yaml
+docker exec kind-control-plane rm /etc/kubernetes/manifests/kube-controller-manager.yaml
+
+docker cp ./configs/controller-mgr/kube-controller-manager-2.yaml kind-control-plane2:/etc/kubernetes/manifests/kube-controller-manager-2.yaml
+docker exec kind-control-plane2 rm /etc/kubernetes/manifests/kube-controller-manager.yaml
+
+docker cp ./configs/controller-mgr/kube-controller-manager-3.yaml kind-control-plane3:/etc/kubernetes/manifests/kube-controller-manager-3.yaml
+docker exec kind-control-plane3 rm /etc/kubernetes/manifests/kube-controller-manager.yaml
